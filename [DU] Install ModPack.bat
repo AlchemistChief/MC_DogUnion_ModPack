07ed323eca3684b8ccee98b418fa73fb336255a6
Mr_Alchemy <meistergamer333@gmail.com>
2025-03-29 23:33:39 +0100

    Add files via upload

--- [DU] Install ModPack.bat
+++ [DU] Install ModPack.bat
@@ -23,16 +23,16 @@ setlocal disableDelayedExpansion
 :: SOFTWARE.
 :: =======================================================================
 
-set "localVersion=30.03.2025/0"
+set "localVersion=30.03.2025/1"
 
 :: =======================================================================
 :: Define ANSI escape sequences for colors
 for /f %%a in ('echo prompt $E ^| cmd') do set "ESC=%%a"
 set "RESET=%ESC%[0m"
 set "PTCOLOR=%ESC%[1;38;5;220m"
-set "ERCOLOR=%ESC%[1;38;5;196m"
-set "WGCOLOR=%ESC%[1;38;5;196m"
-set "DBCOLOR=%ESC%[1;38;5;75m"
+set "ERRCOLOR=%ESC%[1;38;5;196m"
+set "DBGERRCOLOR=%ESC%[1;38;5;196m"
+set "DBGCOLOR=%ESC%[1;38;5;75m"
 set "SSCOLOR=%ESC%[1;38;5;46m"
 
 :: Get the full path of the script
@@ -41,19 +41,19 @@ set "scriptPath=%scriptPath:~0,-1%"
 for /f "delims=" %%i in ('powershell -NoProfile -Command "[System.IO.Path]::GetFullPath('%scriptPath%')"') do set "realPath=%%i"
 for /f "delims=" %%i in ('powershell -Command "(Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/AlchemistChief/MC_DogUnion_ModPack/main/version.json' ).Content | ConvertFrom-Json | Select-Object -ExpandProperty version"') do set "latestVersion=%%i"
 
-echo %DBCOLOR%=============================== [DEBUG] ===============================%RESET%
-echo %DBCOLOR%[DEBUG]%RESET% Local version: %localVersion%
+echo %DBGCOLOR%=============================== [DEBUG] ===============================%RESET%
+echo %DBGCOLOR%[DEBUG]%RESET% Local version: %localVersion%
 if "%latestVersion%"=="%localVersion%" (
-	echo %DBCOLOR%[DEBUG]%RESET% Server versio: %latestVersion%
+	echo %DBGCOLOR%[DEBUG]%RESET% Server versio: %latestVersion%
 ) else (
-    echo %WGCOLOR%[DEBUG]%RESET% A new version is available: %latestVersion%
-    echo %WGCOLOR%[DEBUG]%RESET% Consider updating your script.
+    echo %DBGERRCOLOR%[DEBUG]%RESET% A new version is available: %latestVersion%
+    echo %DBGERRCOLOR%[DEBUG]%RESET% Consider updating your script.
 )
-echo %DBCOLOR%[DEBUG]%RESET% Author: Mr_Alchemy/gunsgamertv
-echo %DBCOLOR%[DEBUG]%RESET% Script is located at:
-echo %DBCOLOR%[DEBUG]%RESET% %scriptPath%
-echo %DBCOLOR%[DEBUG]%RESET% Real path:
-echo %DBCOLOR%[DEBUG]%RESET% %realPath%
+echo %DBGCOLOR%[DEBUG]%RESET% Author: Mr_Alchemy/gunsgamertv
+echo %DBGCOLOR%[DEBUG]%RESET% Script is located at:
+echo %DBGCOLOR%[DEBUG]%RESET% %scriptPath%
+echo %DBGCOLOR%[DEBUG]%RESET% Real path:
+echo %DBGCOLOR%[DEBUG]%RESET% %realPath%
 
 :: Check if the script is in a directory under \mods\
 set "folderName="
@@ -61,27 +61,27 @@ for %%B in ("%realPath%") do set "folderName=%%~nxB"
 for %%A in ("%realPath%") do set "parentDir=%%~dpA"
 
 if /i not "%folderName%"=="mods" (
-    echo %ERCOLOR%=============================== [ERROR] ===============================%RESET%
-    echo %ERCOLOR%[ERROR]%RESET% Error: This script is not located in a directory under \mods\.
+    echo %ERRCOLOR%=============================== [ERROR] ===============================%RESET%
+    echo %ERRCOLOR%[ERROR]%RESET% Error: This script is not located in a directory under \mods\.
     pause
     exit /b
 )
 
 if not exist "%realPath%" (
-    echo %ERCOLOR%=============================== [ERROR] ===============================%RESET%
-    echo %ERCOLOR%[ERROR]%RESET% Error: The directory "%realPath%" does not exist!
+    echo %ERRCOLOR%=============================== [ERROR] ===============================%RESET%
+    echo %ERRCOLOR%[ERROR]%RESET% Error: The directory "%realPath%" does not exist!
     pause
     exit /b
 )
 if not exist "%parentDir%config" (
-    echo %ERCOLOR%=============================== [ERROR] ===============================%RESET%
-    echo %ERCOLOR%[ERROR]%RESET% Error: 'config' folder does not exist in the parent directory!
+    echo %ERRCOLOR%=============================== [ERROR] ===============================%RESET%
+    echo %ERRCOLOR%[ERROR]%RESET% Error: 'config' folder does not exist in the parent directory!
     pause
     exit /b
 )
 if not exist "%parentDir%versions" (
-    echo %ERCOLOR%=============================== [ERROR] ===============================%RESET%
-    echo %ERCOLOR%[ERROR]%RESET% Error: 'versions' folder does not exist in the parent directory!
+    echo %ERRCOLOR%=============================== [ERROR] ===============================%RESET%
+    echo %ERRCOLOR%[ERROR]%RESET% Error: 'versions' folder does not exist in the parent directory!
     pause
     exit /b
 )
@@ -113,87 +113,87 @@ set "extractFiles=%errorlevel%"
 
 :: Download 'Server_Necessary' if chosen
 if %installServer%==1 (
-	echo %DBCOLOR%=============================== [DEBUG] ===============================%RESET%
-	echo %DBCOLOR%[DEBUG]%RESET% Downloading 'Server_Necessary' to: 
-	echo %DBCOLOR%[DEBUG]%RESET% "%serverOutputFile%"
+	echo %DBGCOLOR%=============================== [DEBUG] ===============================%RESET%
+	echo %DBGCOLOR%[DEBUG]%RESET% Downloading 'Server_Necessary' to: 
+	echo %DBGCOLOR%[DEBUG]%RESET% "%serverOutputFile%"
 	powershell -NoProfile -Command "Invoke-WebRequest -Uri '%serverDownLoadLink%' -OutFile '%serverOutputFile%'"
 	if exist "%serverOutputFile%" (
 		echo %SSCOLOR%=============================== [SUCCESS] ===============================%RESET%
 		echo %SSCOLOR%[SUCCESS]%RESET% 'Server_Necessary' download complete.
 	) else (
-		echo %ERCOLOR%=============================== [ERROR] ===============================%RESET%
-		echo %ERCOLOR%[ERROR]%RESET% Error: 'Server_Necessary' download failed.
+		echo %ERRCOLOR%=============================== [ERROR] ===============================%RESET%
+		echo %ERRCOLOR%[ERROR]%RESET% Error: 'Server_Necessary' download failed.
 	)
 )
 
 :: Download 'Client_Recommended' if chosen
 if %installClient%==1 (
-    echo %DBCOLOR%=============================== [DEBUG] ===============================%RESET%
-    echo %DBCOLOR%[DEBUG]%RESET% Downloading 'Client_Recommended' to: 
-    echo %DBCOLOR%[DEBUG]%RESET% "%clientOutputFile%"
+    echo %DBGCOLOR%=============================== [DEBUG] ===============================%RESET%
+    echo %DBGCOLOR%[DEBUG]%RESET% Downloading 'Client_Recommended' to: 
+    echo %DBGCOLOR%[DEBUG]%RESET% "%clientOutputFile%"
     powershell -NoProfile -Command "Invoke-WebRequest -Uri '%clientDownLoadLink%' -OutFile '%clientOutputFile%'"
     if exist "%clientOutputFile%" (
         echo %SSCOLOR%=============================== [SUCCESS] ===============================%RESET%
         echo %SSCOLOR%[SUCCESS]%RESET% 'Client_Recommended' download complete.
     ) else (
-        echo %ERCOLOR%=============================== [ERROR] ===============================%RESET%
-        echo %ERCOLOR%[ERROR]%RESET% 'Client_Recommended' download failed.
+        echo %ERRCOLOR%=============================== [ERROR] ===============================%RESET%
+        echo %ERRCOLOR%[ERROR]%RESET% 'Client_Recommended' download failed.
     )
 )
 
 :: Check if 'options.txt' exists and download if not
 if %installSettings%==1 (
-    echo %DBCOLOR%=============================== [DEBUG] ===============================%RESET%
-    echo %DBCOLOR%[DEBUG]%RESET% Downloading 'options.txt' to: 
-    echo %DBCOLOR%[DEBUG]%RESET% "%optionsFile%"
+    echo %DBGCOLOR%=============================== [DEBUG] ===============================%RESET%
+    echo %DBGCOLOR%[DEBUG]%RESET% Downloading 'options.txt' to: 
+    echo %DBGCOLOR%[DEBUG]%RESET% "%optionsFile%"
     powershell -NoProfile -Command "Invoke-WebRequest -Uri '%optionsDownLoadLink%' -OutFile '%optionsFile%'"
     if exist "%optionsFile%" (
         echo %SSCOLOR%=============================== [SUCCESS] ===============================%RESET%
         echo %SSCOLOR%[SUCCESS]%RESET% 'options.txt' download complete.
     ) else (
-        echo %ERCOLOR%=============================== [ERROR] ===============================%RESET%
-        echo %ERCOLOR%[ERROR]%RESET% Error: 'options.txt' download failed.
+        echo %ERRCOLOR%=============================== [ERROR] ===============================%RESET%
+        echo %ERRCOLOR%[ERROR]%RESET% Error: 'options.txt' download failed.
     )
 )
 :: Download 'optionsof.txt' if Optifine settings are chosen and Client is installed
 if "%installOptifineSettings%"=="1" if "%installClient%"=="1" (
-    echo %DBCOLOR%=============================== [DEBUG] ===============================%RESET%
-    echo %DBCOLOR%[DEBUG]%RESET% Downloading 'optionsof.txt' to: 
-    echo %DBCOLOR%[DEBUG]%RESET% "%optionsOfFile%"
+    echo %DBGCOLOR%=============================== [DEBUG] ===============================%RESET%
+    echo %DBGCOLOR%[DEBUG]%RESET% Downloading 'optionsof.txt' to: 
+    echo %DBGCOLOR%[DEBUG]%RESET% "%optionsOfFile%"
     powershell -NoProfile -Command "Invoke-WebRequest -Uri '%optionsofDownLoadLink%' -OutFile '%optionsOfFile%'"
     if exist "%optionsOfFile%" (
         echo %SSCOLOR%=============================== [SUCCESS] ===============================%RESET%
         echo %SSCOLOR%[SUCCESS]%RESET% 'optionsof.txt' download complete.
     ) else (
-        echo %ERCOLOR%=============================== [ERROR] ===============================%RESET%
-        echo %ERCOLOR%[ERROR]%RESET% 'optionsof.txt' download failed.
+        echo %ERRCOLOR%=============================== [ERROR] ===============================%RESET%
+        echo %ERRCOLOR%[ERROR]%RESET% 'optionsof.txt' download failed.
     )
 )
 
 :: Extract files
 if %extractFiles%==1 (
-	echo %DBCOLOR%=============================== [DEBUG] ===============================%RESET%
+	echo %DBGCOLOR%=============================== [DEBUG] ===============================%RESET%
     if exist "%serverOutputFile%" (
-        echo %DBCOLOR%[DEBUG]%RESET% Extracting 'Server_Necessary.zip'
+        echo %DBGCOLOR%[DEBUG]%RESET% Extracting 'Server_Necessary.zip'
         for /f "delims=" %%f in ('powershell -NoProfile -Command "Expand-Archive -Path '%serverOutputFile%' -DestinationPath '%realPath%' -Force"') do (
             if not exist "%realPath%\%%f" (
                 powershell -NoProfile -Command "Expand-Archive -Path '%serverOutputFile%' -DestinationPath '%realPath%'"
-                echo %DBCOLOR%[DEBUG]%RESET% Extracting '%%f'...
+                echo %DBGCOLOR%[DEBUG]%RESET% Extracting '%%f'...
             ) else (
-                echo %DBCOLOR%[DEBUG]%RESET% Skipping existing file: '%%f'
+                echo %DBGCOLOR%[DEBUG]%RESET% Skipping existing file: '%%f'
             )
         )
         del "%serverOutputFile%"
     )
     
     if exist "%clientOutputFile%" (
-        echo %DBCOLOR%[DEBUG]%RESET% Extracting 'Client_Recommended.zip'
+        echo %DBGCOLOR%[DEBUG]%RESET% Extracting 'Client_Recommended.zip'
         for /f "delims=" %%f in ('powershell -NoProfile -Command "Expand-Archive -Path '%clientOutputFile%' -DestinationPath '%realPath%' -Force"') do (
             if not exist "%realPath%\%%f" (
                 powershell -NoProfile -Command "Expand-Archive -Path '%clientOutputFile%' -DestinationPath '%realPath%'"
-                echo %DBCOLOR%[DEBUG]%RESET% Extracting '%%f'...
+                echo %DBGCOLOR%[DEBUG]%RESET% Extracting '%%f'...
             ) else (
-                echo %DBCOLOR%[DEBUG]%RESET% Skipping existing file: '%%f'
+                echo %DBGCOLOR%[DEBUG]%RESET% Skipping existing file: '%%f'
             )
         )
         del "%clientOutputFile%"
@@ -202,7 +202,7 @@ if %extractFiles%==1 (
     echo %SSCOLOR%=============================== [SUCCESS] ===============================%RESET%
     echo %SSCOLOR%[SUCCESS]%RESET% Extraction and cleanup complete.
 	echo %SSCOLOR%[SUCCESS]%RESET% Mods successfully downloaded and installed.
-    echo =======================================================================
+    echo %SSCOLOR%=======================================================================%RESET%
 )
 
 pause
