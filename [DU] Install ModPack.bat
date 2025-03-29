a9b6eea5de6810b2f2da5e82c6c1b1aa049b4f18
Mr_Alchemy <meistergamer333@gmail.com>
2025-03-30 00:34:01 +0100

    Custom ProgressBar + Faster Downlaoding Speed

--- [DU] Install ModPack.bat
+++ [DU] Install ModPack.bat
@@ -23,18 +23,17 @@ setlocal disableDelayedExpansion
 :: SOFTWARE.
 :: =======================================================================
 
-set "localVersion=30.03.2025/3"
+set "localVersion=30.03.2025/8"
 set "baseGitHubURL=https://raw.githubusercontent.com/AlchemistChief/MC_DogUnion_ModPack/main"
 
 :: =======================================================================
 :: Define ANSI escape sequences for colors
 for /f %%a in ('echo prompt $E ^| cmd') do set "ESC=%%a"
 set "RESET=%ESC%[0m"
-set "PTCOLOR=%ESC%[1;38;5;220m"
-set "ERRCOLOR=%ESC%[1;38;5;196m"
-set "DBGERRCOLOR=%ESC%[1;38;5;196m"
-set "DBGCOLOR=%ESC%[1;38;5;75m"
-set "SSCOLOR=%ESC%[1;38;5;46m"
+set "GOLDCOLOR=%ESC%[1;38;5;220m"
+set "REDCOLOR=%ESC%[1;38;5;196m"
+set "BLUECOLOR=%ESC%[1;38;5;75m"
+set "GREENCOLOR=%ESC%[1;38;5;46m"
 
 :: Get the full path of the script
 set "scriptPath=%~dp0"
@@ -42,19 +41,20 @@ set "scriptPath=%scriptPath:~0,-1%"
 for /f "delims=" %%i in ('powershell -NoProfile -Command "[System.IO.Path]::GetFullPath('%scriptPath%')"') do set "realPath=%%i"
 for /f "delims=" %%i in ('powershell -NoProfile -Command "(Invoke-WebRequest -Uri '%baseGitHubURL%/version.json' ).Content | ConvertFrom-Json | Select-Object -ExpandProperty version"') do set "latestVersion=%%i"
 
-echo %DBGCOLOR%=============================== [DEBUG] ===============================%RESET%
-echo %DBGCOLOR%[DEBUG]%RESET% Local version: %localVersion%
+echo %BLUECOLOR%=============================== [DEBUG] ===============================%RESET%
+echo %BLUECOLOR%[DEBUG]%RESET% Local version: %localVersion%
 if "%latestVersion%"=="%localVersion%" (
-	echo %DBGCOLOR%[DEBUG]%RESET% Server versio: %latestVersion%
+	echo %BLUECOLOR%[DEBUG]%RESET% Server versio: %latestVersion%
 ) else (
-    echo %DBGERRCOLOR%[DEBUG]%RESET% A new version is available: %latestVersion%
-    echo %DBGERRCOLOR%[DEBUG]%RESET% Consider updating your script.
+    echo %REDCOLOR%[DEBUG]%RESET% A new version is available: %latestVersion%
+    echo %REDCOLOR%[DEBUG]%RESET% Consider updating your script.
 )
-echo %DBGCOLOR%[DEBUG]%RESET% Author: Mr_Alchemy/gunsgamertv
-echo %DBGCOLOR%[DEBUG]%RESET% Script is located at:
-echo %DBGCOLOR%[DEBUG]%RESET% %scriptPath%
-echo %DBGCOLOR%[DEBUG]%RESET% Real path:
-echo %DBGCOLOR%[DEBUG]%RESET% %realPath%
+echo %BLUECOLOR%[DEBUG]%RESET% Author: Mr_Alchemy/gunsgamertv
+echo %BLUECOLOR%[DEBUG]%RESET% ProgressBar: %GREENCOLOR%Custom%RESET%
+echo %BLUECOLOR%[DEBUG]%RESET% Script is located at:
+echo %BLUECOLOR%[DEBUG]%RESET% %scriptPath%
+echo %BLUECOLOR%[DEBUG]%RESET% Real path:
+echo %BLUECOLOR%[DEBUG]%RESET% %realPath%
 
 :: Check if the script is in a directory under \mods\
 set "folderName="
@@ -62,38 +62,38 @@ for %%B in ("%realPath%") do set "folderName=%%~nxB"
 for %%A in ("%realPath%") do set "parentDir=%%~dpA"
 
 if /i not "%folderName%"=="mods" (
-    echo %ERRCOLOR%=============================== [ERROR] ===============================%RESET%
-    echo %ERRCOLOR%[ERROR]%RESET% Error: This script is not located in a directory under \mods\.
+    echo %REDCOLOR%=============================== [ERROR] ===============================%RESET%
+    echo %REDCOLOR%[ERROR]%RESET% Error: This script is not located in a directory under \mods\.
     pause
     exit /b
 )
 
 if not exist "%realPath%" (
-    echo %ERRCOLOR%=============================== [ERROR] ===============================%RESET%
-    echo %ERRCOLOR%[ERROR]%RESET% Error: The directory "%realPath%" does not exist!
+    echo %REDCOLOR%=============================== [ERROR] ===============================%RESET%
+    echo %REDCOLOR%[ERROR]%RESET% Error: The directory "%realPath%" does not exist!
     pause
     exit /b
 )
 if not exist "%parentDir%config" (
-    echo %ERRCOLOR%=============================== [ERROR] ===============================%RESET%
-    echo %ERRCOLOR%[ERROR]%RESET% Error: 'config' folder does not exist in the parent directory!
+    echo %REDCOLOR%=============================== [ERROR] ===============================%RESET%
+    echo %REDCOLOR%[ERROR]%RESET% Error: 'config' folder does not exist in the parent directory!
     pause
     exit /b
 )
 if not exist "%parentDir%versions" (
-    echo %ERRCOLOR%=============================== [ERROR] ===============================%RESET%
-    echo %ERRCOLOR%[ERROR]%RESET% Error: 'versions' folder does not exist in the parent directory!
+    echo %REDCOLOR%=============================== [ERROR] ===============================%RESET%
+    echo %REDCOLOR%[ERROR]%RESET% Error: 'versions' folder does not exist in the parent directory!
     pause
     exit /b
 )
 
-for /f "delims=" %%i in ('powershell -NoProfile -Command "(Invoke-WebRequest -Uri '%baseGitHubURL%/links.json').Content | ConvertFrom-Json | Select-Object -ExpandProperty serverDownLoadLink"') do set "serverDownLoadLink=%%i"
+for /f "delims=" %%i in ('powershell -NoProfile -Command "$ProgressPreference = 'SilentlyContinue'; (Invoke-WebRequest -Uri '%baseGitHubURL%/links.json').Content | ConvertFrom-Json | Select-Object -ExpandProperty serverDownLoadLink"') do set "serverDownLoadLink=%%i"
 
-for /f "delims=" %%i in ('powershell -NoProfile -Command "(Invoke-WebRequest -Uri '%baseGitHubURL%/links.json').Content | ConvertFrom-Json | Select-Object -ExpandProperty clientDownLoadLink"') do set "clientDownLoadLink=%%i"
+for /f "delims=" %%i in ('powershell -NoProfile -Command "$ProgressPreference = 'SilentlyContinue'; (Invoke-WebRequest -Uri '%baseGitHubURL%/links.json').Content | ConvertFrom-Json | Select-Object -ExpandProperty clientDownLoadLink"') do set "clientDownLoadLink=%%i"
 
-for /f "delims=" %%i in ('powershell -NoProfile -Command "(Invoke-WebRequest -Uri '%baseGitHubURL%/links.json').Content | ConvertFrom-Json | Select-Object -ExpandProperty optionsDownLoadLink"') do set "optionsDownLoadLink=%%i"
+for /f "delims=" %%i in ('powershell -NoProfile -Command "$ProgressPreference = 'SilentlyContinue'; (Invoke-WebRequest -Uri '%baseGitHubURL%/links.json').Content | ConvertFrom-Json | Select-Object -ExpandProperty optionsDownLoadLink"') do set "optionsDownLoadLink=%%i"
 
-for /f "delims=" %%i in ('powershell -NoProfile -Command "(Invoke-WebRequest -Uri '%baseGitHubURL%/links.json').Content | ConvertFrom-Json | Select-Object -ExpandProperty optionsofDownLoadLink"') do set "optionsofDownLoadLink=%%i"
+for /f "delims=" %%i in ('powershell -NoProfile -Command "$ProgressPreference = 'SilentlyContinue'; (Invoke-WebRequest -Uri '%baseGitHubURL%/links.json').Content | ConvertFrom-Json | Select-Object -ExpandProperty optionsofDownLoadLink"') do set "optionsofDownLoadLink=%%i"
 
 :: Set download file paths before prompting for the choice
 set "clientOutputFile=%realPath%\Client_Recommended.zip"
@@ -101,110 +101,110 @@ set "serverOutputFile=%realPath%\Server_Necessary.zip"
 set "optionsFile=%parentDir%options.txt"
 set "optionsOfFile=%parentDir%optionsof.txt"
 
-echo %PTCOLOR%=============================== [PROMPT] ===============================%RESET%
-choice /C YN /M "%PTCOLOR%[PROMPT]%RESET% Do you want to install 'Server_Necessary' (NEEDED TO JOIN)?"
+echo %GOLDCOLOR%=============================== [PROMPT] ===============================%RESET%
+choice /C YN /M "%GOLDCOLOR%[PROMPT]%RESET% Do you want to install 'Server_Necessary' (NEEDED TO JOIN)?"
 set "installServer=%errorlevel%"
-choice /C YN /M "%PTCOLOR%[PROMPT]%RESET% Do you want to install 'Client_Recommended'?"
+choice /C YN /M "%GOLDCOLOR%[PROMPT]%RESET% Do you want to install 'Client_Recommended'?"
 set "installClient=%errorlevel%"
-choice /C YN /M "%PTCOLOR%[PROMPT]%RESET% Do you want to install preset Optifine Settings? (Only if 'Client_Recommended' = true)"
+choice /C YN /M "%GOLDCOLOR%[PROMPT]%RESET% Do you want to install preset Optifine Settings? (Only if 'Client_Recommended' = true)"
 set "installOptifineSettings=%errorlevel%"
-choice /C YN /M "%PTCOLOR%[PROMPT]%RESET% Do you want to install preset settings?"
+choice /C YN /M "%GOLDCOLOR%[PROMPT]%RESET% Do you want to install preset settings?"
 set "installSettings=%errorlevel%"
-choice /C YN /M "%PTCOLOR%[PROMPT]%RESET% Do you want to automatically extract the zip files and delete them?"
+choice /C YN /M "%GOLDCOLOR%[PROMPT]%RESET% Do you want to automatically extract the zip files and delete them?"
 set "extractFiles=%errorlevel%"
 
 :: Download 'Server_Necessary' if chosen
 if %installServer%==1 (
-	echo %DBGCOLOR%=============================== [DEBUG] ===============================%RESET%
-	echo %DBGCOLOR%[DEBUG]%RESET% Downloading 'Server_Necessary' to: 
-	echo %DBGCOLOR%[DEBUG]%RESET% "%serverOutputFile%"
-	powershell -NoProfile -Command "Invoke-WebRequest -Uri '%serverDownLoadLink%' -OutFile '%serverOutputFile%'"
+	echo %BLUECOLOR%=============================== [DEBUG] ===============================%RESET%
+	echo %BLUECOLOR%[DEBUG]%RESET% Downloading 'Server_Necessary' to: 
+	echo %BLUECOLOR%[DEBUG]%RESET% "%serverOutputFile%"
+	powershell -NoProfile -Command "$url = '%serverDownLoadLink%'; $output = '%serverOutputFile%'; $webClient = New-Object System.Net.WebClient; $webClient.DownloadFileAsync($url, $output); while ($webClient.IsBusy) { Start-Sleep -Seconds 2; $fileSizeBytes = (Get-Item $output).Length; $fileSizeMB = [math]::Round($fileSizeBytes / 1MB, 2); Write-Host '%BLUECOLOR%[DEBUG]%RESET% Downloaded%GOLDCOLOR%' $fileSizeMB '%RESET%MB'; }"
 	if exist "%serverOutputFile%" (
-		echo %SSCOLOR%=============================== [SUCCESS] ===============================%RESET%
-		echo %SSCOLOR%[SUCCESS]%RESET% 'Server_Necessary' download complete.
+		echo %GREENCOLOR%=============================== [SUCCESS] ===============================%RESET%
+		echo %GREENCOLOR%[SUCCESS]%RESET% 'Server_Necessary' download complete.
 	) else (
-		echo %ERRCOLOR%=============================== [ERROR] ===============================%RESET%
-		echo %ERRCOLOR%[ERROR]%RESET% Error: 'Server_Necessary' download failed.
+		echo %REDCOLOR%=============================== [ERROR] ===============================%RESET%
+		echo %REDCOLOR%[ERROR]%RESET% Error: 'Server_Necessary' download failed.
 	)
 )
 
 :: Download 'Client_Recommended' if chosen
 if %installClient%==1 (
-    echo %DBGCOLOR%=============================== [DEBUG] ===============================%RESET%
-    echo %DBGCOLOR%[DEBUG]%RESET% Downloading 'Client_Recommended' to: 
-    echo %DBGCOLOR%[DEBUG]%RESET% "%clientOutputFile%"
-    powershell -NoProfile -Command "Invoke-WebRequest -Uri '%clientDownLoadLink%' -OutFile '%clientOutputFile%'"
+    echo %BLUECOLOR%=============================== [DEBUG] ===============================%RESET%
+    echo %BLUECOLOR%[DEBUG]%RESET% Downloading 'Client_Recommended' to: 
+    echo %BLUECOLOR%[DEBUG]%RESET% "%clientOutputFile%"
+    powershell -NoProfile -Command "$url = '%clientDownLoadLink%'; $output = '%clientOutputFile%'; $webClient = New-Object System.Net.WebClient; $webClient.DownloadFileAsync($url, $output); while ($webClient.IsBusy) { Start-Sleep -Seconds 2; $fileSizeBytes = (Get-Item $output).Length; $fileSizeMB = [math]::Round($fileSizeBytes / 1MB, 2); Write-Host '%BLUECOLOR%[DEBUG]%RESET% Downloaded%GOLDCOLOR%' $fileSizeMB '%RESET%MB'; }"
     if exist "%clientOutputFile%" (
-        echo %SSCOLOR%=============================== [SUCCESS] ===============================%RESET%
-        echo %SSCOLOR%[SUCCESS]%RESET% 'Client_Recommended' download complete.
+        echo %GREENCOLOR%=============================== [SUCCESS] ===============================%RESET%
+        echo %GREENCOLOR%[SUCCESS]%RESET% 'Client_Recommended' download complete.
     ) else (
-        echo %ERRCOLOR%=============================== [ERROR] ===============================%RESET%
-        echo %ERRCOLOR%[ERROR]%RESET% 'Client_Recommended' download failed.
+        echo %REDCOLOR%=============================== [ERROR] ===============================%RESET%
+        echo %REDCOLOR%[ERROR]%RESET% 'Client_Recommended' download failed.
     )
 )
 
 :: Check if 'options.txt' exists and download if not
 if %installSettings%==1 (
-    echo %DBGCOLOR%=============================== [DEBUG] ===============================%RESET%
-    echo %DBGCOLOR%[DEBUG]%RESET% Downloading 'options.txt' to: 
-    echo %DBGCOLOR%[DEBUG]%RESET% "%optionsFile%"
-    powershell -NoProfile -Command "Invoke-WebRequest -Uri '%optionsDownLoadLink%' -OutFile '%optionsFile%'"
+    echo %BLUECOLOR%=============================== [DEBUG] ===============================%RESET%
+    echo %BLUECOLOR%[DEBUG]%RESET% Downloading 'options.txt' to: 
+    echo %BLUECOLOR%[DEBUG]%RESET% "%optionsFile%"
+    powershell -NoProfile -Command "$url = '%optionsDownLoadLink%'; $output = '%optionsFile%'; $webClient = New-Object System.Net.WebClient; $webClient.DownloadFileAsync($url, $output); while ($webClient.IsBusy) { Start-Sleep -Seconds 2; $fileSizeBytes = (Get-Item $output).Length; $fileSizeMB = [math]::Round($fileSizeBytes / 1MB, 2); Write-Host '%BLUECOLOR%[DEBUG]%RESET% Downloaded%GOLDCOLOR%' $fileSizeMB '%RESET%MB'; }"
     if exist "%optionsFile%" (
-        echo %SSCOLOR%=============================== [SUCCESS] ===============================%RESET%
-        echo %SSCOLOR%[SUCCESS]%RESET% 'options.txt' download complete.
+        echo %GREENCOLOR%=============================== [SUCCESS] ===============================%RESET%
+        echo %GREENCOLOR%[SUCCESS]%RESET% 'options.txt' download complete.
     ) else (
-        echo %ERRCOLOR%=============================== [ERROR] ===============================%RESET%
-        echo %ERRCOLOR%[ERROR]%RESET% Error: 'options.txt' download failed.
+        echo %REDCOLOR%=============================== [ERROR] ===============================%RESET%
+        echo %REDCOLOR%[ERROR]%RESET% Error: 'options.txt' download failed.
     )
 )
 :: Download 'optionsof.txt' if Optifine settings are chosen and Client is installed
 if "%installOptifineSettings%"=="1" if "%installClient%"=="1" (
-    echo %DBGCOLOR%=============================== [DEBUG] ===============================%RESET%
-    echo %DBGCOLOR%[DEBUG]%RESET% Downloading 'optionsof.txt' to: 
-    echo %DBGCOLOR%[DEBUG]%RESET% "%optionsOfFile%"
-    powershell -NoProfile -Command "Invoke-WebRequest -Uri '%optionsofDownLoadLink%' -OutFile '%optionsOfFile%'"
+    echo %BLUECOLOR%=============================== [DEBUG] ===============================%RESET%
+    echo %BLUECOLOR%[DEBUG]%RESET% Downloading 'optionsof.txt' to: 
+    echo %BLUECOLOR%[DEBUG]%RESET% "%optionsOfFile%"
+    powershell -NoProfile -Command "$url = '%optionsofDownLoadLink%'; $output = '%optionsOfFile%'; $webClient = New-Object System.Net.WebClient; $webClient.DownloadFileAsync($url, $output); while ($webClient.IsBusy) { Start-Sleep -Seconds 2; $fileSizeBytes = (Get-Item $output).Length; $fileSizeMB = [math]::Round($fileSizeBytes / 1MB, 2); Write-Host '%BLUECOLOR%[DEBUG]%RESET% Downloaded%GOLDCOLOR%' $fileSizeMB '%RESET%MB'; }"
     if exist "%optionsOfFile%" (
-        echo %SSCOLOR%=============================== [SUCCESS] ===============================%RESET%
-        echo %SSCOLOR%[SUCCESS]%RESET% 'optionsof.txt' download complete.
+        echo %GREENCOLOR%=============================== [SUCCESS] ===============================%RESET%
+        echo %GREENCOLOR%[SUCCESS]%RESET% 'optionsof.txt' download complete.
     ) else (
-        echo %ERRCOLOR%=============================== [ERROR] ===============================%RESET%
-        echo %ERRCOLOR%[ERROR]%RESET% 'optionsof.txt' download failed.
+        echo %REDCOLOR%=============================== [ERROR] ===============================%RESET%
+        echo %REDCOLOR%[ERROR]%RESET% 'optionsof.txt' download failed.
     )
 )
 
 :: Extract files
 if %extractFiles%==1 (
-	echo %DBGCOLOR%=============================== [DEBUG] ===============================%RESET%
+	echo %BLUECOLOR%=============================== [DEBUG] ===============================%RESET%
     if exist "%serverOutputFile%" (
-        echo %DBGCOLOR%[DEBUG]%RESET% Extracting 'Server_Necessary.zip'
-        for /f "delims=" %%f in ('powershell -NoProfile -Command "Expand-Archive -Path '%serverOutputFile%' -DestinationPath '%realPath%' -Force"') do (
+        echo %BLUECOLOR%[DEBUG]%RESET% Extracting 'Server_Necessary.zip'
+        for /f "delims=" %%f in ('powershell -NoProfile -Command "$ProgressPreference = 'SilentlyContinue'; Expand-Archive -Path '%serverOutputFile%' -DestinationPath '%realPath%' -Force"') do (
             if not exist "%realPath%\%%f" (
                 powershell -NoProfile -Command "Expand-Archive -Path '%serverOutputFile%' -DestinationPath '%realPath%'"
-                echo %DBGCOLOR%[DEBUG]%RESET% Extracting '%%f'...
+                echo %BLUECOLOR%[DEBUG]%RESET% Extracting '%%f'...
             ) else (
-                echo %DBGCOLOR%[DEBUG]%RESET% Skipping existing file: '%%f'
+                echo %BLUECOLOR%[DEBUG]%RESET% Skipping existing file: '%%f'
             )
         )
         del "%serverOutputFile%"
     )
     
     if exist "%clientOutputFile%" (
-        echo %DBGCOLOR%[DEBUG]%RESET% Extracting 'Client_Recommended.zip'
-        for /f "delims=" %%f in ('powershell -NoProfile -Command "Expand-Archive -Path '%clientOutputFile%' -DestinationPath '%realPath%' -Force"') do (
+        echo %BLUECOLOR%[DEBUG]%RESET% Extracting 'Client_Recommended.zip'
+        for /f "delims=" %%f in ('powershell -NoProfile -Command "$ProgressPreference = 'SilentlyContinue'; Expand-Archive -Path '%clientOutputFile%' -DestinationPath '%realPath%' -Force"') do (
             if not exist "%realPath%\%%f" (
                 powershell -NoProfile -Command "Expand-Archive -Path '%clientOutputFile%' -DestinationPath '%realPath%'"
-                echo %DBGCOLOR%[DEBUG]%RESET% Extracting '%%f'...
+                echo %BLUECOLOR%[DEBUG]%RESET% Extracting '%%f'...
             ) else (
-                echo %DBGCOLOR%[DEBUG]%RESET% Skipping existing file: '%%f'
+                echo %BLUECOLOR%[DEBUG]%RESET% Skipping existing file: '%%f'
             )
         )
         del "%clientOutputFile%"
     )
     
-    echo %SSCOLOR%=============================== [SUCCESS] ===============================%RESET%
-    echo %SSCOLOR%[SUCCESS]%RESET% Extraction and cleanup complete.
-	echo %SSCOLOR%[SUCCESS]%RESET% Mods successfully downloaded and installed.
-    echo %SSCOLOR%=======================================================================%RESET%
+    echo %GREENCOLOR%=============================== [SUCCESS] ===============================%RESET%
+    echo %GREENCOLOR%[SUCCESS]%RESET% Extraction and cleanup complete.
+	echo %GREENCOLOR%[SUCCESS]%RESET% Mods successfully downloaded and installed.
+    echo %GREENCOLOR%=======================================================================%RESET%
 )
 
 pause
